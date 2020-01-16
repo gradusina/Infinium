acdab26093332ead5eb2a5f1c45b3875ba12edad
Andrei Romanchuk <romanchukgrad@gmail.com>
2020-01-16 17:47:06 +0300

    2

--- Modules/WorkAssignments/WorkAssignments.cs
+++ Modules/WorkAssignments/WorkAssignments.cs
@@ -15,7 +15,6 @@ namespace Infinium.Modules.WorkAssignments
     public class TafelSplitStruct
     {
         public int PosCount;
-        public int PosCount1;
         public int SourceCount;
         public int FirstCount;
         public int SecondCount;
